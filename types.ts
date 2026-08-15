f346d1717122751b0239a84eadf699b5b2d36f18
p7agency <p7agency.contato@gmail.com>
2026-08-15 17:58:46 -0300

    feat: adiciona tipos MentoriaSubItem e campos isMentoria/subItems

--- types.ts
+++ types.ts
@@ -1,10 +1,17 @@
 
+export interface MentoriaSubItem {
+  title: string;
+  description: string;
+}
+
 export interface ServiceItem {
   id: string;
   title: string;
   code: string;
   description: string;
   highlight?: boolean;
+  isMentoria?: boolean;
+  subItems?: MentoriaSubItem[];
 }
 
 export interface SectionContent {
